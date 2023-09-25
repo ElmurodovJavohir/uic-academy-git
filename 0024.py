17de1bcdd351a5b8c8df1aae1a7831a326b484d2
Javohir Elmurodov <elmurodovjavohir@gmail.com>
2023-09-25 20:14:49 +0500

    adding robocontest problem solution

---
+++ 0024.py
@@ -0,0 +1,2 @@
+print((h1 := list(map(int, input().split()))) and (h2 := list(map(int, input().split())))
+      and (h2[0]*3600 + h2[1]*60 + h2[2] - h1[0]*3600 - h1[1]*60 - h1[2]))
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+# Topshiriq 1
+# 1 1 1 - 1 soat 1 minut 1 sekund
+# 2 2 2 - 2 soat 2 minut 2 sekund
+
+h1, m1, s1 = map(int, input().split())
+h2, m2, s2 = map(int, input().split())
+print(h2*3600 + m2*60 + s2 - h1*3600 - m1*60 - s1)
